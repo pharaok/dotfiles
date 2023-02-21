ec871f87f77a49c41152f2bb03e8b0d75c524c93
pharaok <farouk.tamer03@gmail.com>
2023-02-22 01:48:45 +0200

    nvim: stop buffers from hijacking toggleterm window

--- .config/nvim/lua/pharaok/plugins/editor.lua
+++ .config/nvim/lua/pharaok/plugins/editor.lua
@@ -85,19 +85,81 @@ return {
 
   {
     "akinsho/toggleterm.nvim",
-    version = "*",
-    keys = { "<C-\\>" },
+    -- version = "*",
+    keys = function(plugin)
+      return { plugin.opts.open_mapping }
+    end,
     opts = {
       start_in_insert = false,
       shading_factor = "-10",
       open_mapping = "<C-\\>",
-      on_open = function()
+      on_create = function(t)
+        t:send('alias nvim="command nvim --server ' .. vim.v.servername .. ' --remote"')
+        vim.b.toggleterm_winnr = t.window
+      end,
+      on_open = function(t)
         vim.cmd([[ startinsert ]])
-        if util.has("trouble.nvim") then
+        if t.direction == "horizontal" and util.has("trouble.nvim") then
           require("trouble").close()
         end
       end,
     },
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+
+      vim.api.nvim_create_autocmd("BufEnter", {
+        nested = true,
+        callback = function()
+          print("BufEnter")
+          local alt_buf = vim.fn.bufnr("#")
+          if alt_buf == -1 then
+            return
+          end
+
+          local filetype = vim.api.nvim_buf_get_option(alt_buf, "filetype")
+          if filetype ~= "toggleterm" then
+            return
+          end
+          local t_win = vim.api.nvim_buf_get_var(alt_buf, "toggleterm_winnr")
+          if t_win ~= vim.api.nvim_get_current_win() then
+            return
+          end
+
+          local eventignore = vim.opt.eventignore
+          vim.opt.eventignore = "all"
+
+          local buf = vim.api.nvim_get_current_buf()
+          local wins = find_buf_wins(buf)
+          local new = table.maxn(wins) == 1
+
+          vim.cmd("buffer #")
+
+          if not new then
+            local win = find_buf_wins(buf)[1]
+            vim.api.nvim_set_current_win(win)
+            vim.opt.eventignore = eventignore
+            return
+          end
+
+          local path = vim.api.nvim_buf_get_name(buf)
+          vim.cmd("bdelete " .. buf)
+
+          local suitable_window
+          print(vim.inspect(vim.api.nvim_tabpage_list_wins(0)))
+          for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+            local win_buf = vim.api.nvim_win_get_buf(win)
+            local win_buf_ft = vim.api.nvim_buf_get_option(win_buf, "filetype")
+            if win_buf_ft ~= "toggleterm" then
+              suitable_window = win
+            end
+          end
+          vim.api.nvim_set_current_win(suitable_window)
+
+          vim.opt.eventignore = eventignore
+          vim.cmd("e " .. vim.fn.fnameescape(path))
+        end,
+      })
+    end,
   },
   {
     "folke/trouble.nvim",
