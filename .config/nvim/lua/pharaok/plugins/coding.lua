04a739133a98630470a82082b5367d72a4080075
pharaok <farouk.tamer03@gmail.com>
2023-11-09 18:22:48 +0200

    nvim: switch nvim_cmp confirm to <Tab>

--- .config/nvim/lua/pharaok/plugins/coding.lua
+++ .config/nvim/lua/pharaok/plugins/coding.lua
@@ -4,15 +4,15 @@ return {
     version = "1.*",
     build = "make install_jsregexp",
     keys = {
-      {
-        "<Tab>",
-        function()
-          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<Tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = "i",
-      },
+      -- {
+      --   "<Tab>",
+      --   function()
+      --     return require("luasnip").expand_or_jumpable(1) and "<Plug>luasnip-jump-next" or "<Tab>"
+      --   end,
+      --   expr = true,
+      --   silent = true,
+      --   mode = "i",
+      -- },
       {
         "<Tab>",
         function()
@@ -98,7 +98,18 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+            elseif require("luasnip").expand_or_jumpable() then
+              vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+            else
+              fallback()
+            end
+          end, {
+            "i",
+            "s",
+          }),
         }),
         formatting = { format = lspkind.cmp_format({ mode = vim.g.icons and "symbol" or "text" }) },
       }
