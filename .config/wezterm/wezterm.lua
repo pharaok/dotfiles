895daf16a6489a71606372d49be1c0664db36a10
pharaok <farouk.tamer03@gmail.com>
2024-09-09 20:00:30 +0300

    wezterm: wsl

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,20 @@
 local wezterm = require("wezterm")
 
-return {
-  font = wezterm.font_with_fallback({ "FiraCode Nerd Font", "Symbols Nerd Font Mono" }),
-  adjust_window_size_when_changing_font_size = false,
-  hide_tab_bar_if_only_one_tab = true,
-  color_scheme = "tokyonight_storm",
-  window_background_opacity = 0.75,
-}
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("FiraCode Nerd Font")
+config.adjust_window_size_when_changing_font_size = false
+config.hide_tab_bar_if_only_one_tab = true
+config.color_scheme = "tokyonight_storm"
+config.window_background_opacity = 0.75
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  config.win32_system_backdrop = "Acrylic"
+
+  local wsl_domain = wezterm.default_wsl_domains()[1]
+  if wsl_domain ~= nil then
+    config.default_domain = wsl_domain.name
+  end
+end
+
+return config
